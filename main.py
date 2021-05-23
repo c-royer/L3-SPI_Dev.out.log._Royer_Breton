ade973898b3f205ce8ade0520af47a1e71bcaa96
MaGn3tiK <60353172+MaGn3tiK@users.noreply.github.com>
2021-05-23 15:40:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 import numpy as np
 import sounddevice as sd
 from signals_generator import *
-from Plot_2D import *
+from interface import *
 from pyqtgraph.Qt import QtCore
 
 
-
 if __name__ == '__main__':
-    p = Plot2D()
-    i = 0
-    frequency = 500
 
-    def update():
+    print('-' * 30)
+    print("Select a method (Signal / Microphone) ? [s/m]")
+    print('-' * 30)
 
-        global p, i
+    method = input()
 
-        t, s = sinus(500, 1, i ) # tu change la fonction ici, et aussi en dessous pour le .wav
-        #certaine méthode ne sont pas au point comme le chirp et le tone burst, ici, la durée est de 1 seconde, et en dessous c'est 50.
+    if method == 's':
 
-        fft_freq, fft_data = FFT(s, 44100)
+        p = Plot2D()
+        i = 0
+        frequency = 500
+
+        def update():
+
+            global p, i
+
+            t, s =  chirp(200, 2000, 1, i)  # tu change la fonction ici, et aussi en dessous pour le .wav
+            #certaine méthode ne sont pas au point comme le chirp et le tone burst, ici, la durée est de 1 seconde, et en dessous c'est 50.
+
+            fft_freq, fft_data = FFT(s, 44100)
+
+            p.trace("waveform", t, s, frequency)
+            p.trace("spectrum", fft_freq, fft_data, frequency)
+            i += 0.4
 
 
+        print('-'*30)
+        print("Play the sound? [o/n] ")
+        print('-' * 30)
+        input = input()
 
-        p.trace("waveform", t, s, frequency)
-        p.trace("spectrum", fft_freq, fft_data, frequency)
-        i += 0.4
+        if input == 'o' :
+            sd.play(square(frequency, 50, 2, i)[1], samplerate = 44100)
 
 
+        timer = QtCore.QTimer()
+        timer.timeout.connect(update)
+        timer.start(25)
+
+        p.start()
+
+    else:
+        import microphone
 
 
-    sd.play(sinus(354, 50, i )[1], samplerate = 44100)
-    timer = QtCore.QTimer()
-    timer.timeout.connect(update)
-    timer.start(25)
-
-    p.start()
 
 
 
---
+++ microphone.py
@@ -0,0 +1,155 @@
+import argparse
+import queue
+import sys
+import wave
+
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+import sounddevice as sd
+from scipy.fftpack import fft, ifft
+import color
+from matplotlib.widgets import CheckButtons
+
+plt.close('all')
+
+
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
+
+
+
+channels = [1]  # input channels to plot (default: the first)
+device = None  # input device (numeric ID or substring)
+window = 200  # visible time slot (default: %(default)s ms)
+interval = 30  # minimum time between plot updates (default: %(default)s ms)
+blocksize = 1  # block size (in samples)
+samplerate = None  # sampling rate of audio device
+downsample = 10  # display every Nth sample (default: %(default)s)
+downsample_dft = 1  # isplay every Nth sample (default: %(default)s)
+
+if any(c < 1 for c in channels):
+    print('argument CHANNEL: must be >= 1')
+
+mapping = [c - 1 for c in channels]  # Channel numbers start with 1
+q = queue.Queue()
+q_dft = queue.Queue()
+q_FRF = queue.Queue()
+
+
+def audio_callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    # Fancy indexing with mapping creates a (necessary!) copy:
+    q.put(indata[::downsample, mapping])
+    q_dft.put(indata[::downsample_dft, mapping])
+
+
+def update_plot(frame):
+    """This is called by matplotlib for each plot update.
+    Typically, audio callbacks happen more frequently than plot updates,
+    therefore the queue tends to contain multiple blocks of audio data.
+    """
+    global plotdata
+    while True:
+        try:
+            data = q.get_nowait()
+        except queue.Empty:
+            break
+        shift = len(data)
+        plotdata = np.roll(plotdata, -shift, axis=0)
+        plotdata[-shift:, :] = data
+    for column, line in enumerate(lines):
+        line.set_ydata(plotdata[:, column])
+
+    return lines
+
+
+def update_dft(frame):
+    """This is called by matplotlib for each plot update.
+    Typically, audio callbacks happen more frequently than plot updates,
+    therefore the queue tends to contain multiple blocks of audio data.
+    """
+    global plotdft
+    while True:
+        try:
+            data = q_dft.get_nowait()
+        except queue.Empty:
+            break
+        NDFT = int(samplerate)
+        data = data
+        data = fft(data[:, 0], NDFT)
+        plotdft[:int(samplerate), 0] = np.abs(data)
+    for column, lin in enumerate(line):
+        lin.set_ydata(plotdft[:, column])
+    return line
+
+
+try:
+    if samplerate is None:
+        device_info = sd.query_devices(device, 'input')
+        samplerate = device_info['default_samplerate']
+
+    length = int(window * samplerate / (1000 * downsample))
+    plotdata = np.zeros((length, len(channels)))
+
+    length_FRF = 512
+    plotFRF = np.zeros((length_FRF, len(channels)))
+
+    # sensitivity = float(input('Sensibilité = '))
+
+    length_dft = int(samplerate)
+    plotdft = np.zeros((length_dft, len(channels)))
+
+
+    with plt.rc_context(
+            {'axes.edgecolor': 'white', 'xtick.color': 'white', 'ytick.color': 'white', 'figure.facecolor': 'white'}):
+
+        fig, (ax1, ax2) = plt.subplots(2, figsize=(17, 8.2))
+        lines = ax1.plot(plotdata, color=color.sea[0], lw = 1.1)
+        line = ax2.semilogy(plotdft, color=color.darkorchid[0], lw = 1.1)
+        fig.patch.set_facecolor('k')
+
+        ax1.set_facecolor('k')
+        ax1.axis((0, len(plotdata), -2, 2))
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['top'].set_visible(False)
+
+
+        ax1.grid(True, color='white', alpha=0.25)
+        ax1.tick_params(bottom=False, top=False, labelbottom=False, right=False, left=True, labelleft=True)
+        ax1.set_title('WAVEFORM', color = 'white')
+        ax1.set_ylabel('Amplitude', color = 'white')
+
+
+        ax2.set_facecolor('k')
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['top'].set_visible(False)
+        ax2.set_ylabel('Amplitude', color = 'white')
+        ax2.set_xlabel('Frequency (Hz)', color = 'white')
+        ax2.axis((0, len(plotdft) / 2, 1E-3, 150))
+        ax2.grid(True, color = 'white', alpha  = 0.25)
+
+        ax2.set_title('SPECTRUM', color = 'white')
+
+
+    plt.tight_layout()
+
+
+    stream = sd.InputStream(
+        device=device, channels=max(channels),
+        samplerate=samplerate, callback=audio_callback)
+    ani = FuncAnimation(fig, update_plot, interval=interval, blit=True)
+    anim = FuncAnimation(fig, update_dft, interval=interval, blit=True)
+
+    with stream:
+
+        plt.show()
+except Exception as e:
+    print(type(e).__name__ + ': ' + str(e))
+
